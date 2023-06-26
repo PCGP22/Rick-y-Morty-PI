6d2eef45fae6f594eeb03e283b10cb5b6710130c
Paulo Gutiérrez <PCGP22@gmail.com>
2023-06-26 10:52:44 -0600

    Primer commit a

--- .history/src/components/SearchBar_20230626101402.jsx
+++
@@ -1,8 +0,0 @@
-export default function SearchBar({onSearch}) {
-   return (
-      <div>
-         <input type='search' placeholder="Búsqueda"/>
-         <button onClick={{onSearch}}>Buscar</button>
-      </div>
-   );
-}
